c1e198059c887ed101d2a0eb7d27f6489e2c356b
Anand-Stark <starkyam32@gmail.com>
2023-03-22 11:55:46 +0530

    finalized cart design

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
 // initializing the database
-const sequeqlize = new Sequelize('foodcart','root','220755',{
-    host:'localhost',
-    dialect:'mysql'
+const sequeqlize = new Sequelize('foodcart','root','ANANd#2021',{
+    host:'./foodcart.sqlite',
+    dialect:'sqlite'
 }    ) ;
 
 module.exports = sequeqlize;
