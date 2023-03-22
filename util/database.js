ef50f64fe233f6747fc96843b03e8b2bb7dadea8
Anand-Stark <starkyam32@gmail.com>
2023-03-22 13:15:39 +0530

    signup/login

--- util/database.js
+++ util/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 // initializing the database
 const sequeqlize = new Sequelize('foodcart','root','ANANd#2021',{
     host:'./foodcart.sqlite',
-    dialect:'sqlite'
+    dialect:'sqlite',
+    logging:false,
 }    ) ;
 
 module.exports = sequeqlize;
