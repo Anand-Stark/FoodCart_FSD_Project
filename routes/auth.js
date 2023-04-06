ab2af9e4dd2ad749ff3bb5e7ff480cf7ae6811bf
Anand-Stark <starkyam32@gmail.com>
2023-04-06 15:15:11 +0530

    have to restart authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,6 @@ const routes = express.Router();
 
 const authentication = require('../controllers/auth');
 
-// get /auth-login
-routes.get('/auth-login',authentication.getAuthenticationLogin);
 
-// get /auth-singup
-routes.get('/auth-signup',authentication.getAuthenticationSignup);
-
-// post /auth-login
-routes.post('/auth-login',authentication.postAuthenticationLogin);
 
 module.exports = routes;
