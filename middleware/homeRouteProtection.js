1e5500a98d196e045806a8d9d48326451b6d4476
Anand-Stark <starkyam32@gmail.com>
2023-04-23 14:04:01 +0530

    changes

--- middleware/homeRouteProtection.js
+++ middleware/homeRouteProtection.js
@@ -1,6 +1,6 @@
 module.exports  = (req,res,next) =>{
      
-    if(!req.session.user || !req.session.owner || !req.session.isAdminLogged){
+    if(!req.session.user && !req.session.owner && !req.session.isAdminLogged){
         return res.redirect('/auth-login');
     }
     
