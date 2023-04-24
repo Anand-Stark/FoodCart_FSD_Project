bcd60add500fcd3d89829a9c5a0f832868a37d97
Zeeshan251 <zeeshan202125@gmail.com>
2023-04-24 20:36:58 +0530

    added code

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // initializing the database
-const sequeqlize = new Sequelize('foodcart','root','ANANd#2021',{
+const sequeqlize = new Sequelize('foodcart','root','220755',{
  
     dialect:'mysql',
     logging:false,
